243962ad34fd557b7e74c3dfabcbfce4441e5bae
yicrotkd <yoichirotkd@gmail.com>
2023-10-24 16:45:02 +0900

    accept tagged template args

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,24 @@ import stringArgv from 'string-argv';
 
 const quote = (cmd: TemplateStringsArray, ...args: Array<string | number>) => {
   return cmd.reduce((acc, cur, i) => {
-    return acc + cur + (args[i] || '')
-  }, '')
+    return acc + cur + (args[i] || '');
+  }, '');
 }
 
 const exec = (c: string) => {
   return Bun.spawn(stringArgv(c));
 };
 
-const execWrapper = (cmd: TemplateStringsArray | string) => {
+const execWrapper = (cmd: TemplateStringsArray | string, ...args: Array<string | number>) => {
   if (typeof cmd === 'string') {
     return exec(cmd);
   } else {
-    return exec(quote(cmd));
+    return exec(quote(cmd, ...args));
   }
 };
 
-export const $ = async (cmd: TemplateStringsArray | string) => {
-  const proc = execWrapper(cmd);
+export const $ = async (cmd: TemplateStringsArray | string, ...args: Array<string | number>) => {
+  const proc = execWrapper(cmd, ...args);
   const text = await new Response(proc.stdout).text();
-  return text
+  return text;
 }
